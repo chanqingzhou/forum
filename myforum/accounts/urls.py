12b0d0c5bc6e18c53e5f1759b316b2bc7fff299d
Jack Chen <jackchenyujie.96@gmail.com>
2018-07-27 15:26:58 +0800

    Improve login and registration forms, including better formatting, need to work on reset password form

--- myforum/accounts/urls.py
+++ myforum/accounts/urls.py
@@ -1,10 +1,12 @@
 # accounts/urls.py
 from django.urls import path
-
+from django.conf.urls import url
+from accounts import views as core_views
 from . import views
 app_name='accounts'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    url(r'^signup/$', core_views.signup, name='signup'),
     path('profile/', views.profile, name='profile' ),
+    url(r'^timeline/$', views.timeline, name='timeline'),
 ]
